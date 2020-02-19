a9e404dbd148d1c21f7672dafb4e3c3ae0ccae17
12Tanu <61134727+12Tanu@users.noreply.github.com>
2020-02-19 14:25:35 +0530

    Add files via upload

---
+++ echo.sh
@@ -0,0 +1,16 @@
+echo "Machine Learning"
+echo Machine Learning
+
+text="Machine Learning"
+echo $text
+
+# echo with `\n` new line
+echo -e "Machine \nLearning"
+
+# echo to file
+echo "Machine Learning" > ml.txt
+
+# echo time
+echo `date`
+
+
